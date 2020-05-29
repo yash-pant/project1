a63921f46a206d09833b197ac8b4e7de1d447d25
Yash Pant <yash.pant727@outlook.com>
2020-05-29 15:37:02 -0700

    Fixed bugged code

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
+if not os.getenv("postgres://emllsnhszfqing:76a579bd999dd5a058a4450f7aef9054688897f4db95917b9d57a1d1bfa7638f@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dfht55lhd7uuvh"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -17,7 +17,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("postgres://emllsnhszfqing:76a579bd999dd5a058a4450f7aef9054688897f4db95917b9d57a1d1bfa7638f@ec2-18-210-214-86.compute-1.amazonaws.com:5432/dfht55lhd7uuvh"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
